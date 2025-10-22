cab9a7e7604c97aa82afe58aa84bdab9f1790d25
Maxsimonyan <simonyanmax2015@gmail.com>
2025-10-22 17:47:17 +0400

    Add files via upload

---
+++ code.js
@@ -0,0 +1,46 @@
+let Un = document.getElementById("Un")
+let lol = document.getElementById("lol")
+let lol1 = document.getElementById("lol1")
+let lol2 = document.getElementById("lol2")
+console.log(lol2)
+function tye(){
+    Un.style.display = "block"
+    lol.innerHTML = "Дубай"
+    lol1.innerHTML = "Дуба́й[4] (араб. دُبَيْ‎ [du.bajj]) — крупнейший город Объединённых Арабских Эмиратов и глубоководный морской порт,"
+    lol2.src = "aaaaaaaaaaaaaaaaaaaaaaaaaaa.png";
+    lol2.style.width = "200px"
+}
+function tye1(){
+    Un.style.display = "block"
+    lol.innerHTML = "Индия"
+    lol1.innerHTML = "И́ндия (хинди भारत, англ. India), официальное название — Респу́блика И́ндия (хинди भारत गणराज्य Bhārat Gaṇarājya,"
+    lol2.src = "ind.webp";
+}
+function tye2(){
+    Un.style.display = "block"
+    lol.innerHTML = "Пакистан"
+    lol1.innerHTML = "Пакиста́н (урду پاکِستان‎ [paːkɪˈst̪aːn] — «земля чистых», англ. Pakistan [ˈpækɪstæn] или [pɑːkiˈstɑːn]), полное название "
+    lol2.src = "pak.webp";
+}
+function UnUse(){
+    Un.style.display = "none"
+}
+function tye3(){
+    Un.style.display = "block"
+    lol.innerHTML = "Армения"
+    lol1.innerHTML = "Арме́ния (арм. Հայաստանо файле [hɑjɑsˈtɑn]), официальное название — Респу́блика Арме́ния "
+    lol2.src = "ar.jpg";
+}
+function tye4(){
+    Un.style.display = "block"
+    Un.style.display = "block"
+    lol.innerHTML = "United States"
+    lol1.innerHTML = "The United States of America (USA)"
+    lol2.src = "download2.png";
+}
+function tye5(){
+    Un.style.display = "block"
+    lol.innerHTML = "Italy"
+    lol1.innerHTML = "Italy,[a] officially the Italian Republic,[b] is a country in Southern"
+    lol2.src = "downloadaaa.png";
+}
